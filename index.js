46002295a037fe74737747263420eeed927db8be
Trung Luong <luongxuantrung@gmail.com>
2019-10-09 14:33:14 +0700

    Update index.js
    Fix loadmore threshold

--- index.js
+++ index.js
@@ -343,10 +343,9 @@ const ScrollableTabView = createReactClass({
       contentOffset,
       contentSize
     }) => {
-      const paddingToBottom = 20;
       return (
         layoutMeasurement.height + contentOffset.y >=
-        contentSize.height - paddingToBottom
+        (contentSize.height - contentSize.height * 0.2)
       );
     };
 
